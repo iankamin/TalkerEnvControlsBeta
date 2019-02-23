65f689924a0fb8830eea128870294f910ca603f3
Arody Deleon <arody888@yahoo.com>
2019-02-23 18:10:16 -0500

    Update ConsoleControls.cs

--- TalkerEnvControlsBETA/ConsoleControls.cs
+++ TalkerEnvControlsBETA/ConsoleControls.cs
@@ -48,6 +48,8 @@ namespace TalkerEnvControlsBETA
             cmd.StartInfo.Arguments = "-ssh pi@192.168.1.11 -pw 9404CSE453 ";
             cmd.StartInfo.CreateNoWindow = true;
             cmd.Start();
+            cmd.StandardInput.WriteLine("sudo systemctl stop lircd.service");
+            Thread.Sleep(50);
             cmd.StandardInput.WriteLine("irsend send_once "+remote+remoteKey);
             cmd.StandardInput.WriteLine("exit");
             string output = cmd.StandardOutput.ReadToEnd();
